f0ecb4a1c8f43442858c22443ba71d27dcd8e028
Jace <livingfern1@live.com>
2022-08-01 10:14:40 -0400

    Shift Repos
    Shift Repos

---
+++ package.js
@@ -0,0 +1,13 @@
+/*
+    FileName: package.js
+    License: The Unlicense
+    Copyright: jbs4bmx
+    Website: https://discord.gg/sptaki
+    Name: SuperAFAK
+    Description: AFAK with additional med features.
+    Version: 2.3.0
+    Aki Version: >=2.2.0
+  	Author(s): jbs4bmx
+*/
+const { afak } = require("./src/afak.js");
+module.exports.afak = new afak();
